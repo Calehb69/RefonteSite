9485ada2b99ca851adc32f42cf503385ce6f0c8f
Calehb69 <isarhu@gmail.com>
2022-05-12 17:06:50 +0200

    #FisrtCommit
    Orga de l'espace de travail
    Install de Boostrap
    Créa du menu

---
+++ pages/Global/association.php
@@ -0,0 +1,5 @@
+<?php include("../Commons/header.php") ?>
+Page de l'association
+<?php include("../Commons/footer.php") ?>
+            
+      
---
+++ pages/Global/partenaires.php
@@ -0,0 +1,5 @@
+<?php include("../Commons/header.php") ?>
+Page des partenaires de l'association
+<?php include("../Commons/footer.php") ?>
+            
+      
