3a2c9aec1f1c0f4d15de2ee8278468e806aa921b
Calehb69 <isarhu@gmail.com>
2022-05-19 01:01:23 +0200

    #UPDATE4_LienBDD_PHP

--- utile/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    const COLOR_ASSO = "perso_ColorRoseMenu";
-    const COLOR_PENSIONNAIRE = "perso_ColorOrangeMenu";
-    const COLOR_ACTUS = "perso_ColorVertMenu";
-    const COLOR_CONSEILS = "perso_ColorRougeMenu";
-    const COLOR_CONTACT = "perso_ColorBleuCielMenu";
-?>
